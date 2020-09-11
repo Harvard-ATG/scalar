be150d8dcf157b0021ecdc9693468b8ef8e53d00
dodget <tylor_dodge@harvard.edu>
2020-09-11 13:46:01 -0400

    Refernce this mirador-viewer

--- system/application/views/widgets/mediaelement/jquery.mediaelement.js
+++ system/application/views/widgets/mediaelement/jquery.mediaelement.js
@@ -5539,7 +5539,7 @@ function YouTubeGetID(url){
 			this.parentView.layoutMediaObject();
 			this.parentView.removeLoadingMessage();
       // make sure mirador doesn't overflow its bounds
-      $('.mirador-viewer').css('max-height', $('.mediaContainer').css('max-height'));
+      $('.mirador-viewer', this.mediaObject).css('max-height', $(this.parentView.mediaContainer).css('max-height'));
 
 			return;
 		}
